7e0c9032b6e1556eca2bf655b5729fb57a69b6f1
npbhatter17 <npbhatter17@gmail.com>
2026-06-05 12:13:33 -0700

    Fix CORS origin allowlist bypass via unanchored regex (CVE-2026-37737)

--- sanic_cors/core.py
+++ sanic_cors/core.py
@@ -303,9 +303,9 @@ def try_match_any(inst, patterns):
 def try_match(request_origin, maybe_regex):
     """Safely attempts to match a pattern or string to a request origin."""
     if isinstance(maybe_regex, RegexObject):
-        return re.match(maybe_regex, request_origin)
+        return re.fullmatch(maybe_regex, request_origin)
     elif probably_regex(maybe_regex):
-        return re.match(maybe_regex, request_origin, flags=re.IGNORECASE)
+        return re.fullmatch(maybe_regex, request_origin, flags=re.IGNORECASE)
     else:
         try:
             return request_origin.lower() == maybe_regex.lower()
